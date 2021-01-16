983b91de41676e61c5294b49a28573bbd0df83f2
Ryan Chan <52082100+rac99@users.noreply.github.com>
2021-01-16 12:26:39 -0500

    audio API

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -4,12 +4,12 @@ const axios = require('axios');
 const fs = require('fs');
 const circle = require("circular-json")
 
-const params = new URL(process.env.API_KEY);
+const SOUND_API_KEY = process.env.API_KEY;
 
 // load dictionaries
-let dictionaryStr = fs.readFileSync('./u.txt', 'utf8');
-let dictionary = JSON.parse(dictionaryStr);
-let dictLength = dictionary.length
+// let dictionaryStr = fs.readFileSync('./u.txt', 'utf8');
+// let dictionary = JSON.parse(dictionaryStr);
+// let dictLength = dictionary.length
 
 // function to get random words
 function getRandomWords(numInts) {
@@ -62,8 +62,46 @@ router.get('/images/:numItems', async (req, res, next) => {
 });
 
 // get audio
-router.get('/audio/:numItems', (req, res, next) => {
-  res.json(`heres some images: ${req.params.numItems} images to be exact`)
+router.get('/audio/:numItems', async (req, res, next) => {
+  
+  var number = parseInt(req.params.numItems);
+
+  function getRandomSoundID(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  try { 
+    //console.log(response);
+
+   let sound_info = [];
+
+
+    for (var i = 0; i < number; i++){
+      let soundID = getRandomSoundID(1, 440000);
+      let response = await axios.get(`https://freesound.org/apiv2/sounds/${soundID}/?token=${SOUND_API_KEY}`);
+      sound_info.push({
+        "id": response.data.id,
+        "url": response.data.url, 
+        "name": response.data.width,
+        "description": response.data.height,
+        "created": response.data.download_url,
+        "duration": response.data.duration,
+        "num_downloads": response.data.num_downloads,
+        "avg_rating": response.data.avg_rating
+      });
+    } 
+    //console.log(sound_info);
+
+    res.json(circle.stringify(sound_info));
+
+  } catch (error) {
+    console.log(error)
+  }
+  
+  
+  //res.json(`heres some images: ${req.params.numItems} images to be exact`)
 });
 
 module.exports = router;
