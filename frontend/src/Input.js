19a05346dfaa3c57201dabdc1a13902c269b7951
bonnielu <bonnielu2002@gmail.com>
2021-01-16 10:47:02 -0500

    horizontal checkboxes

--- frontend/src/Input.js
+++ frontend/src/Input.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Checkbox from './Checkbox.js'
 import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container'
+import Container from 'react-bootstrap/Container';
+
 
 const OPTIONS = ["Text", "Images", "Audio"];
 
