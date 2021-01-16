92bb039dd56ea8635ec3e4598e7df0923818ff55
danielq987 <danielq987@gmail.com>
2021-01-16 08:56:50 -0500

    Setting Up Scripts

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,27 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const http = require('http')
 
 // routers
 var apiRouter = require('./routes/api');
 
 var app = express();
+const server = http.createServer(app);
+const port = process.env.PORT || '5000';
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', apiRouter);
+
+server.listen(port, () => {
+  console.log("listening on *:" + port)
+})
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
