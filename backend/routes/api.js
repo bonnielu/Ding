e3351694cffeffca36a85b27be1389529db33364
danielq987 <danielq987@gmail.com>
2021-01-16 00:13:27 -0500

    created a skeleton for the api

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -6,9 +6,19 @@ router.get('/', function (req, res, next) {
   res.send("It works!");
 });
 
-router.get('/words', (req, res, next) => {
-  res.json("ryan chan is a great guy!!");
-})
+// get words
+router.get('/words/:numItems', (req, res, next) => {
+  res.json(`heres some words: ${req.params.numItems} words to be exact`);
+});
 
+// get images
+router.get('/images/:numItems', (req, res, next) => {
+  res.json(`heres some images: ${req.params.numItems} images to be exact`)
+});
+
+// get audio
+router.get('/audio/:numItems', (req, res, next) => {
+  res.json(`heres some images: ${req.params.numItems} images to be exact`)
+});
 
 module.exports = router;
