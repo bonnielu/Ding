48d43e6f1ce283e3c907a9779bf3d9cb3a9a90b3
Ryan Chan <52082100+rac99@users.noreply.github.com>
2021-01-16 02:13:38 -0500

    pictures api draft

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -13,7 +14,54 @@ router.get('/words/:numItems', (req, res, next) => {
 
 // get images
 router.get('/images/:numItems', (req, res, next) => {
-  res.json(`heres some images: ${req.params.numItems} images to be exact`)
+  
+  var number = parseInt(req.params.numItems);
+
+  // this website can display lists depending on the numItems
+  function pleaseWork() {
+    return axios.get(`https://picsum.photos/v2/list?page=2&limit=${number}`).then(res =>res.data)
+  }
+
+  // currently returns all data in JSON
+  pleaseWork()
+  .then(data => {
+    res.json({data})
+  })
+  .catch(err => console.log(err))
+  
+  // some random code that may be needed later
+  /*
+  axios.get(`https://picsum.photos/v2/list?page=2&limit=100`, {
+    })
+    .then((response) => {
+      //console.log(response.data);
+
+      return response.data;
+      //return `data:image/jpeg;base64,${Buffer.from(res.data, "binary").toString("base64")}`
+    })
+    .catch((error) => {
+      console.log(error)
+      res.end()
+    });
+    */
+  // 
+  
+/*
+  var i;
+  for (i = 1; i <= number; i++){
+    //axios.get(`https://picsum.photos/200?random=${req.params.numItems}$`, {
+    axios.get(`https://picsum.photos/200?random=${number}`, {
+    })
+    .then((res) => {
+      res.json()
+    })
+    .catch((error) => {
+      res.json(`error`)
+    });
+
+  //res.json(`heres some images: ${req.params.numItems} images to be exact`)
+  }
+  */
 });
 
 // get audio
