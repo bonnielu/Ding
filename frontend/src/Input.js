c193ee14827bd50d6c4a99a6b44c0b58bcaadfed
bonnielu <bonnielu2002@gmail.com>
2021-01-16 01:30:29 -0500

    checkboxes added

--- frontend/src/Input.js
+++ frontend/src/Input.js
@@ -1,16 +1,70 @@
 import React from 'react';
+import Checkbox from './Checkbox.js'
 
-const Input = () => (
-    <form className="form">
-        <input
-            className="input"
-            type="checkbox"
-            // onChange={(event) => setMessage(event.target.value)}
-            // onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
+const OPTIONS = ["Text", "Images", "Audio"];
+
+class Input extends React.Component {
+    state = {
+        checkboxes: OPTIONS.reduce(
+            (options, option) => ({
+                ...options,
+                [option]: false
+            })
+        )
+    }
+
+    handleCheckboxChange = changeEvent => {
+        const {name} = changeEvent.target;
+    
+        this.setState(prevState => ({
+            checkboxes: {
+                ...prevState.checkboxes,
+                [name]: !prevState.checkboxes[name]
+            }
+        }));
+    };
+    
+    handleFormSubmit = formSubmitEvent => {
+        formSubmitEvent.preventDefault();
+    
+        Object.keys(this.state.checkboxes)
+          .filter(checkbox => this.state.checkboxes[checkbox])
+          .forEach(checkbox => {
+            console.log(checkbox, "is selected.");
+        });
+    };
+
+    createCheckbox = option => (
+        <Checkbox
+          label={option}
+          isSelected={this.state.checkboxes[option]}
+          onCheckboxChange={this.handleCheckboxChange}
+          key={option}
         />
-        <lable>Images</lable>
-        {/* <button className="sendButton" onClick={(event) => sendMessage(event)}>Send</button> */}
-    </form>
-);
+      );
+
+      createCheckboxes = () => OPTIONS.map(this.createCheckbox);
+
+    render() {
+        return (
+            <div className="container">
+            <div className="row mt-5">
+                <div className="col-sm-12">
+                <form onSubmit={this.handleFormSubmit}>
+                    {this.createCheckboxes()}
+        
+                    <div className="form-group mt-2">
+                    <button type="submit" className="btn btn-primary">
+                        Submit
+                    </button>
+                    </div>
+                </form>
+                </div>
+            </div>
+            </div>
+        );
+    }
+};
+
 
 export default Input;
