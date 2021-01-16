154590ac9d0943d0485b45f116fbdb2f1af116f8
Anna Wang <annaw2193@gmail.com>
2021-01-16 02:40:05 -0500

    Creating the skeleton for tag based input

---
+++ frontend/src/InputTags.js
@@ -0,0 +1,26 @@
+// https://medium.com/@jerrylowm/buila-a-tags-input-react-component-from-scratch-1524f02acb9a
+
+import React from "react";
+
+class InputTag extends React.Component {
+    render() {
+// Component Wrapper
+<div className="input-tag">
+    {/* // List of tags */}
+    <ul className="input-tag__tags">
+        {/* // Each tag */}
+        <li>
+            Tag
+            {/* // Remove tag button */}
+            <button type="button">Remove</button>
+        </li>
+
+
+        {/* // Actual Input to Add Tafs - Using <li> to have the input always inline</li> */}
+        <li className="input-tag__tags__input">
+            <input type="text" />
+        </li>
+    </ul>
+</div>
+    }
+}
