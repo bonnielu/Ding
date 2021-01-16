229e87e7a2a0f4800fa007bbeea27aca345dfffc
Anna Wang <annaw2193@gmail.com>
2021-01-16 09:34:42 -0500

    modifying tags

--- frontend/src/InputTags.js
+++ frontend/src/InputTags.js
@@ -2,25 +2,22 @@
 
 import React from "react";
 
-class InputTag extends React.Component {
-    render() {
-// Component Wrapper
-<div className="input-tag">
-    {/* // List of tags */}
-    <ul className="input-tag__tags">
-        {/* // Each tag */}
-        <li>
-            Tag
-            {/* // Remove tag button */}
-            <button type="button">Remove</button>
-        </li>
+const Input_tag = () => (
+    // Component Wrapper
+    <div className="input-tag">
+        {/* // List of tags */}
+        <ul className="input-tag__tags">
+            {/* // Each tag */}
+            <li>
+                Tag
+                {/* // Remove tag button */}
+                <button type="button">Remove</button>
+            </li>
 
-
-        {/* // Actual Input to Add Tafs - Using <li> to have the input always inline</li> */}
-        <li className="input-tag__tags__input">
-            <input type="text" />
-        </li>
-    </ul>
-</div>
-    }
-}
+            {/* // Actual Input to Add Tafs - Using <li> to have the input always inline</li> */}
+            <li className="input-tag__tags__input">
+                <input type="text" />
+            </li>
+        </ul>
+    </div>
+);
