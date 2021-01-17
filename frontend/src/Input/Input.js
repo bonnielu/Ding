0e6feacb987b1455b331da69973ee440a2b77dd0
Anna Wang <annaw2193@gmail.com>
2021-01-16 21:09:12 -0500

    new changes

--- frontend/src/Input/Input.js
+++ frontend/src/Input/Input.js
@@ -48,74 +48,152 @@ class Input extends React.Component {
       key={option}
     />
   );
+    
+    //  Handles form submit events 
+    handleFormSubmit = formSubmitEvent => {
+        formSubmitEvent.preventDefault();
 
-  //  Handles form submit events
-  handleFormSubmit = (formSubmitEvent) => {
-    formSubmitEvent.preventDefault();
+        let imageCheck = false; 
+        let audioCheck = false; 
+        let textCheck = false; 
 
-    let imageCheck = false;
-    let audioCheck = false;
-    let textCheck = false;
+        let numImage = 0;
+        let numAudio = 0;
+        let numText = 0;
+        
+    
+        Object.keys(this.state.checkboxes)
+          .filter(checkbox => this.state.checkboxes[checkbox])
+          .forEach(checkbox => {
+              if (checkbox === 'Images') {
+                  imageCheck = true;
+              }
+              if (checkbox === "Audio") {
+                  audioCheck = true;
+              }
+              if (checkbox === "Text") {
+                  textCheck = true;
+              }
+            });
 
-    let numImage = 0;
-    let numAudio = 0;
-    let numText = 0;
 
-    const audio = new Audio("/ding.mp3");
-    audio.play();
+            // produces the number of elements to be generated
+            if (imageCheck && textCheck && audioCheck) {
+                numImage = Math.floor((Math.random() * this.state.num) + 1);
+                numAudio = Math.floor((Math.random() * (this.state.num - numImage)) + 1);
+                numText = this.state.num - numImage - numAudio;
+            }
+            else if (imageCheck && textCheck && !audioCheck) {
+                numImage = Math.floor((Math.random() * this.state.num) + 1);
+                numText = this.state.num - numImage;
+            }
+            else if (imageCheck && !textCheck && audioCheck) {
+                numImage = Math.floor((Math.random() * this.state.num) + 1);
+                numAudio = this.state.num - numImage;
+            }
+            else if (!imageCheck && textCheck && audioCheck) {
+                numAudio = Math.floor((Math.random() * this.state.num) + 1);
+                numText = this.state.num - numAudio;
+            }
+            else {
+                if (imageCheck) {
+                    numImage = this.state.num;
+                }
+                else if (textCheck) {
+                    numText = this.state.num;
+                }
+                else if (audioCheck) {
+                    numAudio = this.state.num;
+                }
+                else {
+                    this.state.num = 0;
+                }
+            }
 
-    Object.keys(this.state.checkboxes)
-      .filter((checkbox) => this.state.checkboxes[checkbox])
-      .forEach((checkbox) => {
-        if (checkbox === "Images") {
-          imageCheck = true;
-          numImage = Math.floor(Math.random() * this.state.num + 1);
-        }
-        if (checkbox === "Audio") {
-          audioCheck = true;
-          numAudio = Math.floor(
-            Math.random() * (this.state.num - numImage) + 1
-          );
-          console.log(numAudio);
-        }
-        if (checkbox === "Text") {
-          textCheck = true;
-          numText = this.state.num - numImage - numAudio;
-        }
-      });
+            // Call backend for image, word, and text information 
+            if (imageCheck) {
+                axios.get(`http://localhost:5000/images/${numImage}`, {
+                }).then(function(response) {
+                    var download = [];
+                    for (let i = 0; i < numImage; i++) {
+                        download.push(JSON.parse(response['data'])[i]['download_url']);
+                    }
+                    return (this.setState({downloads: download}))
+                }.bind(this))
+            }
+            if (textCheck) {
+                axios.get(`http://localhost:5000/words/${numText}`, {
+                }).then(function(response) {
+                    var wordArray = response['data'];
+                    return (this.setState({words: wordArray}))
+                }.bind(this))
+            }
+            if (audioCheck) {
+                axios.get(`http://localhost:5000/audio/${numAudio}`, {
+                }).then(function(response) {
+                    var audioLink = [];
+                    for (let i = 0; i < numAudio; i++) {
+                        audioLink.push(JSON.parse(response['data'])[i]['preview-lq-mp3']);
+                    }
+                    return (this.setState({audios: audioLink}))
+                }.bind(this))
+            }
+    };
 
-    // Call backend for image, word, and text information
-    if (imageCheck) {
-      axios.get(`http://localhost:5000/images/${numImage}`, {}).then(
-        function (response) {
-          var download = [];
-          for (let i = 0; i < numImage; i++) {
-            download.push(JSON.parse(response["data"])[i]["download_url"]);
-          }
-          return this.setState({ downloads: download });
-        }.bind(this)
-      );
-    }
-    if (textCheck) {
-      axios.get(`http://localhost:5000/words/${numText}`, {}).then(
-        function (response) {
-          var wordArray = response["data"];
-          return this.setState({ words: wordArray });
-        }.bind(this)
-      );
-    }
-    if (audioCheck) {
-      axios.get(`http://localhost:5000/audio/${numAudio}`, {}).then(
-        function (response) {
-          var audioLink = [];
-          for (let i = 0; i < numAudio; i++) {
-            audioLink.push(JSON.parse(response["data"])[i]["preview-lq-mp3"]);
-          }
-          return this.setState({ audios: audioLink });
-        }.bind(this)
-      );
+    // Dynamically create checkboxes
+    createCheckboxes = () => OPTIONS.map(this.createCheckbox);
+
+    render() {
+        return (
+            <div className="container">
+            <div className="row mt-5">
+                <div className="col-sm-12">
+                <form onSubmit={this.handleFormSubmit}>
+                    <div className='checkboxBox'>
+                        {this.createCheckboxes()}
+                    </div>
+
+                    <label>
+                        Number of Page Elements <input type='number' min='1' max='50' step='1' value={this.state.num} onChange={this.handleNumChange}></input>
+                    </label>
+                    
+        
+                    <div className="form-group mt-2">
+                    <Button type="submit" className="btn generate" size='lg' block>
+                        DING
+                    </Button>
+                    {
+                        this.state.downloads.map((image, i) => 
+                            <div key={i}>
+                                <img src={image} alt="DingImage"></img>
+                            </div> 
+                        )
+                    }
+                    {
+                        this.state.words.map((word, i) => 
+                            <div key={i}>
+                                <p>{word}</p>
+                            </div> 
+                        )
+                    }
+                    {
+                        this.state.audios.map((audioLink, i) => 
+                            <div key={i}>
+                                <audio controls>
+                                    <source src={audioLink} type="audio/mp3"></source>
+                                </audio>
+                            </div> 
+                        )
+                    }
+                    </div>
+                </form>
+                </div>
+            </div>
+            </div>
+        );
     }
   };
+
   // Dynamically create checkboxes
   createCheckboxes = () => OPTIONS.map(this.createCheckbox);
 
@@ -167,9 +245,6 @@ class Input extends React.Component {
       </div>
     );
   }
-
-  // Dynamically create checkboxes
-  createCheckboxes = () => OPTIONS.map(this.createCheckbox);
 }
 
 export default Input;
